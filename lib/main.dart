bd2f2a5f50c7066d14312b48bbaf1fa30dcd894a
= <robbycatur330@gmail.com>
2026-04-17 06:19:56 +0700

    Add functionality

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Pretty App',
+      title: 'Flutter Functionality',
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
       home: const MyHomePage(),
     );
   }
 }
+
+// Home Page
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -33,9 +33,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String text = "Hello World";
 
+  bool isFavorite = false;
+
   void updateText() {
     setState(() {
       text = "Button Pressed";
+    });
+  }
+
+  void toggleFavorite() {
+    setState(() {
+      isFavorite = !isFavorite;
     });
   }
 
@@ -43,17 +51,18 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pretty App"),
+        title: const Text("Add Functionality"),
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Colors.white,
       ),
 
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(24.0), // spacing biar lega
+          padding: const EdgeInsets.all(24.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // TEXT
               Text(
                 text,
                 style: Theme.of(context).textTheme.headlineMedium,
@@ -62,15 +71,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
               const SizedBox(height: 30),
 
+              // BUTTON 1 (ubah text)
               ElevatedButton(
                 onPressed: updateText,
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
-                  ),
-                ),
                 child: const Text("Click Me"),
+              ),
+
+              const SizedBox(height: 20),
+
+              // BUTTON 2 (favorite toggle)
+              IconButton(
+                onPressed: toggleFavorite,
+                icon: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: Colors.red,
+                  size: 32,
+                ),
               ),
             ],
           ),
